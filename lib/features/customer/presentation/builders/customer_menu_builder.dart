41ce0b646b2b6a807b6bb17269fb1f486abf92a8
Dev-DeividCosta <dev.deivid.costa@gmail.com>
2026-08-04 15:24:09 -0300

    feat(customer): Módulo de clientes e tela de cadastro (#10)
    * refactor(customer): renomeia tela e extrai menu builder
    
    * feat(customer): implementa tela de cadastro de novo cliente

---
+++ lib/features/customer/presentation/builders/customer_menu_builder.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../../../../core/models/app_menu_item.dart';
+import '../pages/new_customer_screen.dart';
+
+abstract class CustomerMenuBuilder {
+  List<AppMenuItem> getMenuItems(BuildContext context);
+}
+
+class CustomerMenuBuilderImpl implements CustomerMenuBuilder {
+  const CustomerMenuBuilderImpl();
+
+  @override
+  List<AppMenuItem> getMenuItems(BuildContext context) {
+    return [
+      AppMenuItem(
+        title: 'Adicionar novo cliente',
+        icon: Icons.person_add,
+        color: const Color(0xFFB71C1C),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NewCustomerScreen(),
+            ),
+          );
+        },
+      ),
+      AppMenuItem(
+        title: 'Listar clientes cadastrados',
+        icon: Icons.list_alt,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+      AppMenuItem(
+        title: 'Gerar relatório PDF dos clientes',
+        icon: Icons.picture_as_pdf,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+      AppMenuItem(
+        title: 'Saldo devedor de um cliente',
+        icon: Icons.money_off,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+      AppMenuItem(
+        title: 'Pagamentos de um cliente',
+        icon: Icons.payment,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+      AppMenuItem(
+        title: 'Vendas por cliente',
+        icon: Icons.shopping_bag,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+      AppMenuItem(
+        title: 'Lista dos clientes em excel',
+        icon: Icons.table_view,
+        color: const Color(0xFFB71C1C),
+        onTap: () {},
+      ),
+    ];
+  }
+}
